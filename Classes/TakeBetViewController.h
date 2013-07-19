609796721a9496045931cf1a6da05316cceb4a2a
Kenneth Poon <kenneth@allproperty.com.sg>
2013-07-19 14:56:14 +0800

    create bet here with Jason

---
+++ Classes/TakeBetViewController.h
@@ -0,0 +1,13 @@
+//
+//  TakeBetViewController.h
+//  AlphaVanilla2
+//
+//  Created by Kenneth on 19/7/13.
+//  Copyright (c) 2013 Kenneth. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TakeBetViewController : NSObject
+
+@end
---
+++ Classes/TakeBetViewController.m
@@ -0,0 +1,13 @@
+//
+//  TakeBetViewController.m
+//  AlphaVanilla2
+//
+//  Created by Kenneth on 19/7/13.
+//  Copyright (c) 2013 Kenneth. All rights reserved.
+//
+
+#import "TakeBetViewController.h"
+
+@implementation TakeBetViewController
+
+@end
