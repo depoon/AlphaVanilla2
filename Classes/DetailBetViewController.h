c3e8d3501cff2ce6446bdc11521f4ad299e1aefc
Kenneth Poon <kenneth@allproperty.com.sg>
2013-07-19 17:43:56 +0800

    create bet here with Jason

---
+++ Classes/DetailBetViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailBetViewController.h
+//  AlphaVanilla2
+//
+//  Created by Kenneth on 19/7/13.
+//  Copyright (c) 2013 Kenneth. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DetailBetViewController : NSObject
+
+@end
---
+++ Classes/DetailBetViewController.m
@@ -0,0 +1,13 @@
+//
+//  DetailBetViewController.m
+//  AlphaVanilla2
+//
+//  Created by Kenneth on 19/7/13.
+//  Copyright (c) 2013 Kenneth. All rights reserved.
+//
+
+#import "DetailBetViewController.h"
+
+@implementation DetailBetViewController
+
+@end
