a8823b6e3e33454d988a4968164d71c115affba1
Kenneth Poon <kenneth@allproperty.com.sg>
2013-07-19 15:56:22 +0800

    create bet here with Jason

---
+++ Classes/ParseChallengeBetRequestDelegate.h
@@ -0,0 +1,13 @@
+//
+//  ParseChallengeBetRequestDelegate.h
+//  AlphaVanilla2
+//
+//  Created by Kenneth on 19/7/13.
+//  Copyright (c) 2013 Kenneth. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol ParseChallengeBetRequestDelegate <NSObject>
+
+@end
